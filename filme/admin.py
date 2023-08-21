449b7b97c24a1b1f8f952253182175f8d2329748
thi7silva <thiago100antonio@gmail.com>
2023-08-21 10:26:53 -0300

    first commit

---
+++ filme/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Filme, Episodio, Usuario
+from django.contrib.auth.admin import UserAdmin
+
+# só existe pq o admin foi criado um campo vistos
+campos = list(UserAdmin.fieldsets)
+campos.append(
+    ("Histórico", {"fields": ('filmes_vistos',)})
+)
+
+UserAdmin.fieldsets = tuple(campos)
+
+# Register your models here.
+admin.site.register(Filme)
+admin.site.register(Episodio)
+admin.site.register(Usuario, UserAdmin)
---
+++ filme/novos_context.py
@@ -0,0 +1,13 @@
+from .models import Filme
+
+def lista_filmes_recentes(request):
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:8]
+    return {"lista_filmes_recentes": lista_filmes}
+
+def lista_filmes_emalta(request):
+    lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:8]
+    return {"lista_filmes_emalta": lista_filmes}
+
+def filme_destaque(request):
+    filme = Filme.objects.order_by('-data_criacao')[0]
+    return {"filme_destaque": filme}
